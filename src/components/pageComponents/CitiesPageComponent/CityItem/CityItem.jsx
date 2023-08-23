4afd486b5bfc35dfc038bcfa34d434748488b1cd
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2023-08-23 19:47:32 +0300

    commented unused variables

--- src/components/pageComponents/CitiesPageComponent/CityItem/CityItem.jsx
+++ src/components/pageComponents/CitiesPageComponent/CityItem/CityItem.jsx
@@ -1,56 +1,56 @@
-import { useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { useTemperature } from '../../../../hooks/TemperatureContext';
-import { useEffect } from 'react';
-import { toast } from 'react-hot-toast';
-import { requestWeatherByCity } from '../../../../services/API_services';
+// import { useState } from 'react';
+// import { useSearchParams } from 'react-router-dom';
+// import { useTemperature } from '../../../../hooks/TemperatureContext';
+// import { useEffect } from 'react';
+// import { toast } from 'react-hot-toast';
+// import { requestWeatherByCity } from '../../../../services/API_services';
 
 const CityItem = ({ city }) => {
-  const [icon, setIcon] = useState('');
-  const [temperature, setTemperature] = useState('');
-  const [weatherConditions, setWeatherConditions] = useState('');
-  const [status, setStatus] = useState('idle');
-  const [searchParams, setSearchParams] = useSearchParams();
-  const searchCity = searchParams.get('city');
-  const [cityName, setCityName] = useState('');
+  // const [icon, setIcon] = useState('');
+  // const [temperature, setTemperature] = useState('');
+  // const [weatherConditions, setWeatherConditions] = useState('');
+  // const [status, setStatus] = useState('idle');
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const searchCity = searchParams.get('city');
+  // const [cityName, setCityName] = useState('');
 
-  useEffect(() => {
-    const getCurrentWeather = async () => {
-      try {
-        console.log(city);
-        const weatherInfo = await requestWeatherByCity(city);
-        if (weatherInfo.status === 200) {
-          // setStatus('resolved');
-          setCityName(weatherInfo.data.name);
-          setTemperature(weatherInfo.data.main.temp);
-          setIcon(weatherInfo.data.weather[0].icon);
-          // setWeatherConditions(weatherInfo.data.weather[0].main);
-          console.log(weatherInfo);
-        }
-      } catch (error) {
-        if (city) {
-          // searchParams.delete("city");
-          // setSearchParams(searchParams);
-          // setStatus('rejected');
+  // useEffect(() => {
+  //   const getCurrentWeather = async () => {
+  //     try {
+  //       console.log(city);
+  //       const weatherInfo = await requestWeatherByCity(city);
+  //       if (weatherInfo.status === 200) {
+  //         // setStatus('resolved');
+  //         setCityName(weatherInfo.data.name);
+  //         setTemperature(weatherInfo.data.main.temp);
+  //         setIcon(weatherInfo.data.weather[0].icon);
+  //         // setWeatherConditions(weatherInfo.data.weather[0].main);
+  //         console.log(weatherInfo);
+  //       }
+  //     } catch (error) {
+  //       if (city) {
+  //         // searchParams.delete("city");
+  //         // setSearchParams(searchParams);
+  //         // setStatus('rejected');
 
-          if (error.message === 'Request failed with status code 404') {
-            toast.error(
-              'There is no result matching your query. Check its spelling or enter another city',
-              {
-                id: 'error1',
-              }
-            );
-          } else {
-            toast.error(`API error: ${error.message}`, {
-              id: 'error2',
-            });
-          }
-        }
-      }
-    };
+  //         if (error.message === 'Request failed with status code 404') {
+  //           toast.error(
+  //             'There is no result matching your query. Check its spelling or enter another city',
+  //             {
+  //               id: 'error1',
+  //             }
+  //           );
+  //         } else {
+  //           toast.error(`API error: ${error.message}`, {
+  //             id: 'error2',
+  //           });
+  //         }
+  //       }
+  //     }
+  //   };
 
-    getCurrentWeather(city);
-  }, [city]);
+  //   getCurrentWeather(city);
+  // }, [city]);
   return <div></div>;
 };
 
--- src/components/pageComponents/WeatherPageComponent/WeatherPageComponent.styled.js
+++ src/components/pageComponents/WeatherPageComponent/WeatherPageComponent.styled.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const WeatherPageContainer = styled.div`
   @media ${props => props.theme.media.sx} {
